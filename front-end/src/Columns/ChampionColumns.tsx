dc32d9c6f5b27726864bcfa0891e504f14fa41c7
Best Potato <grevoliver@gmail.com>
2025-07-14 15:22:46 +0200

    Show number of skins, best rarity and best skin name in table

--- front-end/src/Columns/ChampionColumns.tsx
+++ front-end/src/Columns/ChampionColumns.tsx
@@ -1,18 +1,21 @@
-import type { Champion } from "@/types/champion";
+import type { Champion, ChampionWithSkin } from "@/types/champion";
 import type { ColumnDef } from "@tanstack/react-table";
 
-export const ChampionColumns: ColumnDef<Champion>[] = [
+export const ChampionColumns: ColumnDef<ChampionWithSkin>[] = [
   {
     header: "Name",
     accessorKey: "name",
   },
   {
     header: "Number of skins",
+    accessorKey: "numberOfSkins",
   },
   {
     header: "Best rarity",
+    accessorFn: (row) => row.bestSkin?.rarity,
   },
   {
     header: "Best skin",
+    accessorFn: (row) => row.bestSkin?.rarity,
   },
 ];
