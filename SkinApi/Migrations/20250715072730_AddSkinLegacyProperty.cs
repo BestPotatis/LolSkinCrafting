ab07ee2544704a7acc85a48094eaba2ed619e6f5
Best Potato <grevoliver@gmail.com>
2025-07-15 09:28:19 +0200

    Add legacy bool to skin

---
+++ SkinApi/Migrations/20250715072730_AddSkinLegacyProperty.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SkinApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSkinLegacyProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Legacy",
+                table: "Skins",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Legacy",
+                table: "Skins");
+        }
+    }
+}
--- SkinApi/Models/Skin.cs
+++ SkinApi/Models/Skin.cs
@@ -8,6 +8,7 @@ public class Skin
     public int Id { get; set; }
     public required string Name { get; set; }
     public RarityEnum Rarity { get; set; }
+    public bool Legacy { get; set; }
     public int ChampionId { get; set; }
     [JsonIgnore]
     public Champion Champion { get; set; }
