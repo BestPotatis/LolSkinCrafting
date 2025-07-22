ab8bf10834d83d82d713f603ac2bb599a3927213
Best Potato <grevoliver@gmail.com>
2025-07-22 22:46:04 +0200

    Add delete selected to skin shard table

--- front-end/src/Components/SkinShardTable.tsx
+++ front-end/src/Components/SkinShardTable.tsx
@@ -7,8 +7,10 @@ import { BASE_URL } from "@/constants";
 import { FormDialog } from "./FormDialog";
 import { CreateSkinShardForm } from "./Forms/CreateSkinShardForm";
 import { useForm } from "react-hook-form";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { toast } from "react-toastify";
+import { ConfirmDialog } from "./ConfirmDialog";
+import { type Table } from "@tanstack/react-table";
 
 export function SkinShardTable() {
   const queryClient = useQueryClient();
@@ -19,37 +21,76 @@ export function SkinShardTable() {
   });
 
   const submitForm = useForm<CreateSkinShard>();
-  const [open, setOpen] = useState(false);
+  const [addSkinOpen, setAddSkinOpen] = useState(false);
+  const [deleteMultipleOpen, setDeleteMultipleOpen] = useState(false);
+  const ref = useRef<{ table: Table<SkinShardHasSkin> }>(null);
 
-  const successToast = () => toast.success("Successfully created skin shard");
+  const addSuccessToast = () =>
+    toast.success("Successfully created skin shard");
+  const deleteSuccessToast = () =>
+    toast.success("Successfully deleted skin shards");
 
   const createSkinShardMutation = useMutation({
     mutationFn: (data: CreateSkinShard) =>
       axios.post(BASE_URL + "/skin-shards/create", data),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["skinShards"] });
-      setOpen(false);
-      successToast();
+      setAddSkinOpen(false);
+      addSuccessToast();
+    },
+  });
+
+  const deleteMultipleMutation = useMutation({
+    mutationFn: (data: { ids: number[] }) =>
+      axios.post(BASE_URL + "/skin-shards/delete-multiple", data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["skinShards"] });
+      setDeleteMultipleOpen(false);
+      deleteSuccessToast();
+      ref.current?.table.setRowSelection({});
     },
   });
 
   return (
     <>
       <DataTable
+        ref={ref}
         title="Skin shards"
         columns={SkinShardColumns}
         data={data}
         tableButtons={
-          <FormDialog
-            buttonText="Add skin shard"
-            title="Add skin shard"
-            description="Add a skin shard for a champion"
-            dialogForm={<CreateSkinShardForm submitForm={submitForm} />}
-            open={open}
-            setOpen={setOpen}
-            submitFn={(data) => createSkinShardMutation.mutate(data)}
-            submitForm={submitForm}
-          />
+          <>
+            <ConfirmDialog
+              buttonText="Delete selected"
+              dataName={
+                ref.current?.table
+                  .getSelectedRowModel()
+                  .rows.map((r) => r.original.name)
+                  .join(", ") ?? ""
+              }
+              open={deleteMultipleOpen}
+              setOpen={setDeleteMultipleOpen}
+              mutateFn={() =>
+                deleteMultipleMutation.mutate({
+                  ids:
+                    ref.current?.table
+                      .getSelectedRowModel()
+                      .rows.map((r) => Number(r.original.id)) ?? [],
+                })
+              }
+            />
+
+            <FormDialog
+              buttonText="Add skin shard"
+              title="Add skin shard"
+              description="Add a skin shard for a champion"
+              dialogForm={<CreateSkinShardForm submitForm={submitForm} />}
+              open={addSkinOpen}
+              setOpen={setAddSkinOpen}
+              submitFn={(data) => createSkinShardMutation.mutate(data)}
+              submitForm={submitForm}
+            />
+          </>
         }
       />
     </>
